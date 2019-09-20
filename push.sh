b2066a0f8262dce12aa6888a04fbcdd1e61a716c
Vitalii Medvedev <vitaliim@onehourtranslation.com>
2019-09-20 15:25:32 +0300

    Initial commit

---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker tag django-app:0.1 vim1projacto/projacto-repo:django
+docker push vim1projacto/projacto-repo:django
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run --name my-django-app -p 8000:8000 -d django-app:0.1
